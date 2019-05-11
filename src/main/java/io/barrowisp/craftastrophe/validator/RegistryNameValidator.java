5929c6ad466a9b3121709220445f58c35dfbf7e7
yooksi <yooksi@zoho.com>
2019-05-11 02:17:41 +0200

    Create registry name validation classes

---
+++ src/main/java/io/barrowisp/craftastrophe/validator/RegistryNameValidator.java
@@ -0,0 +1,45 @@
+package io.barrowisp.craftastrophe.validator;
+
+import io.barrowisp.craftastrophe.common.Defines;
+import io.barrowisp.craftastrophe.defines.ForgeRegIdentifier;
+import io.barrowisp.craftastrophe.util.AnnotationUtils;
+import org.apache.logging.log4j.spi.StandardLevel;
+
+import javax.validation.ConstraintValidator;
+import javax.validation.ConstraintValidatorContext;
+
+public class RegistryNameValidator implements ConstraintValidator<ForgeRegIdentifier, String> {
+
+    private ForgeRegIdentifier constraint;
+
+    @Override
+    public void initialize(ForgeRegIdentifier constraint) {
+        this.constraint = constraint;
+    }
+
+    @Override
+    public boolean isValid(String value, ConstraintValidatorContext ctx) {
+
+        String message = ForgeRegIdentifier.RULE_VIOLATION;
+
+        if (ForgeRegIdentifier.RULE_REGEX.matcher(value).find()) {
+            if (ForgeRegIdentifier.CONVENTION_REGEX.matcher(value).find()) {
+                return true;
+            }
+            else {
+                /* Registry name is valid according to Minecraft standards but doesn't conform
+                 * to mod specific naming conventions so we're gonna just throw a warning here
+                 */
+                AnnotationUtils.changeAnnotationValue(constraint, "level", "WARN");
+                message = ForgeRegIdentifier.CONVENTION_VIOLATION;
+            }
+        }
+        /* Disable current violation and build a new one with a different output
+         * message depending on if the rule or convention was broken
+         */
+        ctx.disableDefaultConstraintViolation();
+        message += " " + ForgeRegIdentifier.SECONDARY_LOG;
+        ctx.buildConstraintViolationWithTemplate(message).addConstraintViolation();
+        return false;
+    }
+}
