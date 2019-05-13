12adc994bb6bec068b0c4fb67a19f27de0dd7816
yooksi <yooksi@zoho.com>
2019-05-13 03:42:12 +0200

    Update CFLogger initialization

--- src/main/java/io/barrowisp/craftastrophe/CFLogger.java
+++ src/main/java/io/barrowisp/craftastrophe/CFLogger.java
@@ -25,7 +25,7 @@ public final class CFLogger {
     static void init() {
 
         if (logger == null)
-            logger = ModLogger.create(Craftastrophe.class);
+            logger = ModLogger.create(Craftastrophe.MODID);
         else
             logger.warn("Trying to initialize mod logger more then once");
     }
